cf2b75725691efb291063cdfd07362fb103f2c89
Max Rosenboom <maxrosenboom@gmail.com>
2023-09-28 20:22:44 -0600

    adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head> 
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"></link>
+        <link rel="shortcut icon" type="image/x-icon" href="https://maxrosenboom.github.io/portfolio-site/favicon.ico"></link>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
